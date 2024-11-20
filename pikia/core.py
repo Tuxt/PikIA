193ceb8b8445686f014f05128b5639076e26064e
Tuxt <wm.tuxt@gmail.com>
2024-11-20 18:30:58 +0100

    feat: add class to manage detection labels and weighting

--- pikia/core.py
+++ pikia/core.py
@@ -43,3 +43,72 @@ class PikIA:
         directories = list(set(directories))
 
         return directories
+
+
+
+class ObjectDetection:
+    def __init__(self, tag, bbox, img_shape):
+        self.tag = tag
+        self.img_shape = img_shape
+        self.bbox = bbox
+    
+    @property
+    def img_shape(self):
+        return self._img_shape
+
+    @img_shape.setter
+    def img_shape(self, value):
+        self._img_shape = value
+        self.img_area = self.calc_area((0, 0, *value))
+        self.max_img_distance = self.calc_point_distance((0, 0), value)
+
+    @property
+    def bbox(self):
+        return self._bbox
+
+    @bbox.setter
+    def bbox(self, bbox):
+        self._bbox = bbox
+        self.calc_weight()
+
+
+    def calc_weight(self):
+        self.calc_area_weight()
+        self.calc_centrality_weight()
+        self.weight = self.area_weight * self.centrality_weight
+    
+    def calc_area_weight(self):
+        bbox_area = self.calc_area(self._bbox)
+        self.area_weight = bbox_area / self.img_area
+    
+    def calc_centrality_weight(self):
+        self.bbox_distance = ObjectDetection.calc_bbox_distance(
+            self.bbox, (0, 0, *self.img_shape)
+        )
+        self.centrality_weight = 1 - (self.bbox_distance / self.max_img_distance)
+        
+    
+    @staticmethod
+    def calc_area(bbox):
+        x1, y1, x2, y2 = bbox
+        return (x2 - x1) * (y2 - y1)
+
+    @staticmethod
+    def calc_center(bbox):
+        x1, y1, x2, y2 = bbox
+        return ((x2 + x1) / 2 , (y2 + y1) / 2)
+
+    @staticmethod
+    def calc_point_distance(p1, p2):
+        x1, y1 = p1
+        x2, y2 = p2
+        return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+    
+    @staticmethod
+    def calc_bbox_distance(bbox1, bbox2):
+        p1 = ObjectDetection.calc_center(bbox1)
+        p2 = ObjectDetection.calc_center(bbox2)
+        return ObjectDetection.calc_point_distance(p1, p2)
+
+    def __str__(self):
+        return f"ObjectDetection({self.tag}, {self.weight})"
