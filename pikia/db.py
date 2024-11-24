5732c60d952f9da94c4cfc1e3202df120af7454d
Tuxt <wm.tuxt@gmail.com>
2024-11-24 03:03:42 +0100

    feat(db): add functions for inserting images, labels, and relationships
    - Implemented `insert_imagefiles` to insert a list of image file paths into the database.
    - Implemented `insert_labels` to insert a list of labels into the database.
    - Implemented `insert_file_label_relation` to insert a relationship between image files and labels.
      - Includes validation to ensure both image and label exist before inserting the relationship.
      - Raises `RelationshipError` if the relationship cannot be established.

--- pikia/db.py
+++ pikia/db.py
@@ -3,6 +3,8 @@ import sqlite3
 connection: sqlite3.Connection = sqlite3.connect("pikia.db")
 cursor: sqlite3.Cursor = connection.cursor()
 
+class RelationshipError(Exception):
+    pass
 
 def create_db() -> None:
     cursor.execute(
@@ -36,3 +38,25 @@ def create_db() -> None:
 
 
 create_db()
+
+
+def insert_imagefiles(filenames: list[str]):
+    filenames = [(filename,) for filename in filenames]
+    cursor.executemany("INSERT INTO files(filepath) VALUES (?)", filenames)
+    connection.commit()
+
+def insert_labels(labels: list[str]):
+    labels = [(label,) for label in labels]
+    cursor.executemany("INSERT OR IGNORE INTO labels(labelname) VALUES (?)", labels)
+    connection.commit()
+
+def insert_file_label_relation(filename: str, label: str, weight: int):
+    result = cursor.execute("SELECT files.id, labels.id FROM files, labels WHERE files.filepath = ? AND labels.labelname = ?", [filename, label]).fetchone()
+    if result is None:
+        raise RelationshipError(f"Missing row for file '{filename}' or label '{label}'")
+    
+    file_id, label_id = result
+    cursor.execute("INSERT INTO file_label(file_id, label_id, weight) VALUES (?, ?, ?)", [file_id, label_id, weight])
+    connection.commit()
+
+    
