2a82ae5cf3a801a24a10753fb47ce0b9b2e14fdd
Tuxt <wm.tuxt@gmail.com>
2024-12-14 10:34:08 +0100

    fix: correct typo in function name
    - Renamed function `insert_final_labels` to `update_final_labels` to fix a typo.

--- pikia/core.py
+++ pikia/core.py
@@ -53,7 +53,7 @@ class PikIA:
 
         # Save definitive labels to db
         final_labels = db.select_images_with_best_label(clusters)
-        db.insert_final_labels([(e[2], e[0]) for e in final_labels])    # Arg: list with pairs (label_id, file_id)
+        db.update_final_labels([(e[2], e[0]) for e in final_labels])    # Arg: list with pairs (label_id, file_id)
 
     def _prompt_directories(self):
         # Instructions
